12b0fd943222000726a8ed1d64404ec7f856d0cf
Raja Soundaramourty <rajasoun@cisco.com>
2022-04-24 23:59:15 +0530

    feat(cloud-nuke): for self study

--- cloud-nuke/nuke.sh
+++ cloud-nuke/nuke.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-AWS_PROFILE="personal-account"
 AWS_IDENTITY_COMMAND="$(git rev-parse --show-toplevel)/cost-explorer/libs/identity.py"
 AWS_NUKE_COMMAND="cloud-nuke aws --region us-east-1 --config nuke.yml"
 
 $AWS_IDENTITY_COMMAND 
-$AWS_NUKE_COMMAND && AWS_VAULT=
+$AWS_NUKE_COMMAND 
 
