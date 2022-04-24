e80100187fef12ade31e8e60c7e889f6032ae368
Raja Soundaramourty <rajasoun@cisco.com>
2022-04-24 07:57:15 +0000

    fix(lint): black autolint

--- cost-explorer/bill.py
+++ cost-explorer/bill.py
@@ -16,8 +16,10 @@ import logging
 
 # AWS
 import boto3.session
+
 # .env File Loading
 from dotenv import load_dotenv
+
 # Data
 import pandas as pd
 from tabulate import tabulate
@@ -38,21 +40,22 @@ class Bill:
     def __init__(self):
         """Inirialization"""
         parser = argparse.ArgumentParser()
-        parser.add_argument('--log', type=str, default='debug')
-        parser.add_argument('--days', type=int, default=30)
-        parser.add_argument('--profile', type=str, default='default')
-        parser.add_argument('--current_month', type=str,
-                            default="false")  # Previous Month
+        parser.add_argument("--log", type=str, default="debug")
+        parser.add_argument("--days", type=int, default=30)
+        parser.add_argument("--profile", type=str, default="default")
+        parser.add_argument(
+            "--current_month", type=str, default="false"
+        )  # Previous Month
         args = parser.parse_args()
 
         # Convert to upper case to allow the user to
         # specify --log=DEBUG or --log=debug
         log_level = getattr(logging, args.log.upper(), None)
         if not isinstance(log_level, int):
-            msg = 'Invalid log level: %s'
+            msg = "Invalid log level: %s"
             raise ValueError(msg % args.log)
         logging.basicConfig(level=log_level)
-        self.logger = logging.getLogger('cost-explorer')
+        self.logger = logging.getLogger("cost-explorer")
 
         self.is_current_month = args.current_month
         if self.is_current_month.lower() == "true":
@@ -62,16 +65,17 @@ class Bill:
 
         self.aws_profile = args.profile
         now = datetime.datetime.utcnow()
-        self.start = (now - datetime.timedelta(days=args.days)
-                      ).strftime('%Y-%m-%d')
-        self.end = now.strftime('%Y-%m-%d')
+        self.start = (now - datetime.timedelta(days=args.days)).strftime("%Y-%m-%d")
+        self.end = now.strftime("%Y-%m-%d")
         load_dotenv()
 
     def get_aws_session(self, aws_profile):
         """Get AWS Session"""
-        session = boto3.session.Session(profile_name=aws_profile,
-                                        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-                                        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+        session = boto3.session.Session(
+            profile_name=aws_profile,
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        )
         return session
 
 
@@ -79,12 +83,15 @@ def format_report(costexplorer):
     """Format Report Array"""
     lines = []
     for report in costexplorer.reports:
-        print("\n" + report['Name'])
-        lines.append(tabulate(pd.read_csv(costexplorer.csv_file_name),
-                              headers='keys',
-                              tablefmt='psql',
-                              showindex=False)
-                     )
+        print("\n" + report["Name"])
+        lines.append(
+            tabulate(
+                pd.read_csv(costexplorer.csv_file_name),
+                headers="keys",
+                tablefmt="psql",
+                showindex=False,
+            )
+        )
     return lines
 
 
@@ -94,15 +101,16 @@ def main():
     session = bill.get_aws_session(bill.aws_profile)
     whoami_info = identity.whoami(session=session)
     print(identity.format_whoami(whoami_info))
-    client = session.client('ce', 'us-east-1')
+    client = session.client("ce", "us-east-1")
     costexplorer = CostExplorer(client, CurrentMonth=False)
-    costexplorer.addReport(Name="Total", GroupBy=[],
-                           Style='Total', IncSupport=True, type='chart')
+    costexplorer.addReport(
+        Name="Total", GroupBy=[], Style="Total", IncSupport=True, type="chart"
+    )
     reports = format_report(costexplorer)
     for report in reports:
         print(report)
     costexplorer.generate_excel(CURRENT_MONTH=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
