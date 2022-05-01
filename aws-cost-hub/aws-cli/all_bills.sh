38bcc33d4d0d34c1010facbf0df01c2f8d9055e4
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-01 07:16:11 +0000

    fix(aws-cost-hub): aws billing script path

--- aws-cost-hub/aws-cli/all_bills.sh
+++ aws-cost-hub/aws-cli/all_bills.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+function report_path(){
+    base_path="/tmp"
+    # Is Git Directory
+    if [ $(git rev-parse --is-inside-work-tree > /dev/null 2>&1) ]; then
+        # Is report directory in .gitignore
+        if [  $(git check-ignore -v reports | grep -c "reports") ];then
+            base_path="${PWD}"
+        fi
+    fi
+    report_dir="${base_path}/reports/aws-cost-hub/aws-cli"
+    if [ ! -d $report_dir ];then
+        mkdir -p $report_dir
+    fi
+    echo $report_dir
+}
+
 case "$OSTYPE" in
     # MacOS
     darwin*)
@@ -9,9 +25,9 @@ case "$OSTYPE" in
     *);;
 esac
 
-echo "Account ID,Account Alias,Bill" > ${PWD}/aws-cost-hub/aws-cli/bill.csv
+echo "Account ID,Account Alias,Bill" > $(report_path)/bill.csv
 for aws_profile in $(aws configure list-profiles);do
     echo -e "Getting Bill for $aws_profile"
     export AWS_PROFILE=$aws_profile
-    .devcontainer/.aws/aws_vault_env.sh "aws-cost-hub/aws-cli/bill.sh"
+    .devcontainer/.aws/aws_vault_env.sh "aws-cost-hub/aws-cli/bill.sh $(report_path)/bill.csv"
 done
